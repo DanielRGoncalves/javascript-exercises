383c61c28fe7f2b0905ff8ee3180468612e98c92
DanielRGoncalves <daniel.goncalvesvv@gmail.com>
2026-02-22 09:30:33 -0300

    repeatandreverse

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
 
+  let resultado = ''; 
+
+  for (let i = 0; i < num; i++) {
+    resultado = resultado + string; 
+  }
+
+  return resultado; 
 };
 
+// Teste rápido para você ver no terminal
+console.log(repeatString('hey', 3));
 // Do not edit below this line
 module.exports = repeatString;
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+    
+    let resultado = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+       resultado = resultado + string[i];
+    }
+    return resultado;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+console.log(reverseString('hello there'));
